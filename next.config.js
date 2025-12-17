83155eee862ffc3adb708a6fc07ae4a7b948510d
kimyoungjae96 <sskim5421@gmail.com>
2025-12-17 13:34:16 +0900

    feat: deploy 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  basePath: '/pontislabs-website',
+  assetPrefix: '/pontislabs-website/',
+  images: {
+    unoptimized: true,
+  },
+}
 
 module.exports = nextConfig
