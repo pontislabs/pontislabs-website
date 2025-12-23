90a881f2878f60e3185d6e512893883298e481a5
96kimyoungjae <sskim5421@gmail.com>
2025-12-23 23:07:26 +0900

    fix: path

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/pontislabs-website',
-  assetPrefix: '/pontislabs-website/',
   images: {
     unoptimized: true,
   },
